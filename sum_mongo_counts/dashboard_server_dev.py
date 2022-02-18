bbf78f7b31c9fcb40203ef10a04b50dce17e1457
eric <gorskie@moravian.edu>
2022-02-18 12:49:52 -0500

    WIP. implemented sum_mongo_counts in the dashboard server file

---
+++ sum_mongo_counts/dashboard_server_dev.py
@@ -0,0 +1,67 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+from redis import Redis
+from pymongo import MongoClient
+from sum_mongo_counts import connect_mongo_db, get_done_counts
+import docker
+
+
+class Dashboard:
+    def __init__(self, redis_server, docker_server, mongo_client):
+        self.app = Flask(__name__)
+        self.redis = redis_server
+        self.docker = docker_server
+        self.mongo = mongo_client
+        CORS(self.app, resources={r'/data': {'origins': '*'}})
+
+
+def get_jobs_stats(database):
+    jobs_waiting = int(database.llen('jobs_waiting_queue'))
+    jobs_in_progress = int(database.hlen('jobs_in_progress'))
+    jobs_done = int(get_done_counts(database.mongo, 'mirrulations'))
+    jobs_total = jobs_waiting + jobs_in_progress + jobs_done
+    client_ids = database.get('total_num_client_ids')
+    clients_total = int(client_ids) if client_ids is not None else 0
+    return {
+        'num_jobs_waiting': jobs_waiting,
+        'num_jobs_in_progress': jobs_in_progress,
+        'num_jobs_done': jobs_done,
+        'jobs_total': jobs_total,
+        'clients_total': clients_total
+    }
+
+
+def get_container_stats(client):
+    stats = {}
+    for container in client.containers.list():
+        long_name_lst = container.name.split('_')
+        long_name_lst.pop(0)
+        long_name_lst.pop(-1)
+        name = '_'.join(long_name_lst)
+        status = container.status
+        stats[name] = status
+    return stats
+
+
+def create_server(database, docker_server, mongo_client):
+    dashboard = Dashboard(database, docker_server, mongo_client)
+
+    @dashboard.app.route('/', methods=['GET'])
+    def _index():
+        return render_template(
+            'index.html'
+        )
+
+    @dashboard.app.route('/data', methods=['GET'])
+    def _get_dashboard_data():
+        data = get_jobs_stats(dashboard.redis)
+        container_information = get_container_stats(dashboard.docker)
+        data.update(container_information)
+        return jsonify(data), 200
+
+    return dashboard
+
+
+if __name__ == '__main__':
+    server = create_server(Redis('redis'), docker.from_env(), connect_mongo_db('mongo'))
+    server.app.run(port=5000)
--- sum_mongo_counts/sum_mongo_counts.py
+++ sum_mongo_counts/sum_mongo_counts.py
@@ -1,3 +1,4 @@
+
 """
 This module contains the functions that will be used to query the mongoDB
 
@@ -7,8 +8,13 @@ Dependencies:
 from pymongo import MongoClient
 
 
-def connect_mongo_db():
-    client = MongoClient('localhost', 27017)
+def connect_mongo_db(host_name, port_number):
+    # Todo: move hostname into .env
+    if host_name is None:
+        host_name = 'localhost'
+    if port_number is None:
+        port_number = '27017'
+    client = MongoClient(host_name, port_number)
     try:
         print(client.server_info())
 
@@ -18,12 +24,29 @@ def connect_mongo_db():
     return client
 
 
-def get_done_counts(db):
-    dockets_count = int(db['mirrulations']['dockets'].count_documents({}))
-    docs_count = int(db['mirrulations']['documents'].count_documents({}))
-    comments_count = int(db['mirrulations']['comments'].count_documents({}))
+# def load_db_from_disk(client, db_name):
+#     """ 
+#     loads data from disk. 
+#     do NOT commit that data to the repo! 
+
+#     to find 'data_loc' do the following:
+#     1.  connect to mongo via terminal and use any db
+#     2.  type db.serverCmdLineOpts()
+#     3.  look for the value stored in the "parsed/storage/dbPath" attributes.
+#         ex. "dbPath" : "/usr/local/var/mongodb" (this path should be correct for homebrew users)
+#     4. move the data to this path
+#     """
+
+    
+
+
+def get_done_counts(client, db_name):
+    dockets_count = int(client[db_name]['dockets'].count_documents({}))
+    docs_count = int(client[db_name]['documents'].count_documents({}))
+    comments_count = int(client[db_name]['comments'].count_documents({}))
     return dockets_count + docs_count + comments_count
 
 
+
 client = connect_mongo_db()
-print(get_done_counts(client))
+print(get_done_counts(client, "mirrulations"))
